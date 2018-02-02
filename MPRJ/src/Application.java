a5c3ea7f9ddc4b807d120b0c6a277a9d3cccd2b0
Alanne Soares <alannesilva@yahoo.com.br>
2018-02-02 03:21:14 -0200

    adicionado na Application o replace de Link

--- MPRJ/src/Application.java
+++ MPRJ/src/Application.java
@@ -12,6 +12,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Application {
 
@@ -50,27 +51,113 @@ public class Application {
                             "\nClasse: " + descrClasse.getTextContent() +
                             "\nÓrgão Julgador: " + orgaoJulgador.getTextContent()
             );
-        }
-
-         catch (MalformedURLException e) { e.printStackTrace(); }
-
-            catch (Exception e) {
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
 
             File file = new File("index.html");
-            URL url = null;
-            url = new URL(consulta);
+            URL url = new URL(consulta);
             new LoadPage().getPage(url, file);
 
+            /*DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+            Document index = docBuilder.parse(new File("index.html"));*/
+
+            /*FileReader arq;
+            BufferedReader lerArq;
+            String linha;
+
+            if (file != null) {
+                arq = new FileReader(file);
+                lerArq = new BufferedReader(arq);
+                linha = lerArq.readLine();         //lê primeira linha
+
+                while (linha != null) {
+                    //System.out.println(linha);
+
+                    linha = lerArq.readLine();     //lê da segunda linha pra frente
+                }
+
+                arq.close();*/
+                /*DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+                Document index = docBuilder.parse(new File("index.html"));
+            Element element = (Element) index.getDocumentElement();
+            Element link = (Element) element.getElementsByTagName("<link>").item(0);
+                Element tag = (Element) element.getElementsByTagName("<link></link>").item(0);
+            Node tagLink = (Node) link.replaceChild(link, tag);*/
+
+            /*FileReader ler = new FileReader("index.html");
+            BufferedReader leitor = new BufferedReader(ler);
+            String linha;
+            String linhaReescrita;
+            while((linha = leitor.readLine())!= null) {
+                System.out.println(linha);
+                linhaReescrita = linha.replaceAll(linha, "Novo texto");
+                System.out.println("\n" + linhaReescrita);
+            }
+        }*/
+            OutputStream bytes = new FileOutputStream("index.html", true); // passado "true" para gravar no mesmo arquivo
+            OutputStreamWriter chars = new OutputStreamWriter(bytes);
+            BufferedWriter strings = new BufferedWriter(chars);
+
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(false);
             DocumentBuilder docBuilder = dbf.newDocumentBuilder();
             Document index = docBuilder.parse(new File("index.html"));
-                Element element = index.getDocumentElement();
-                element.getTagName().replaceAll("<link href='http://www.tjrj.jus.br/importacao-portal/css' type='text/css' rel='stylesheet'>", "<link href='http://www.tjrj.jus.br/importacao-portal/css' type='text/css' rel='stylesheet'></link>");
-                //NodeList node = element.getElementsByTagName("link").
 
-            //Element form = (Element) index.getElementById("form");
-            //Element a = (Element) index.getElementsByTagName("a").item(1);
+            Element element = index.getDocumentElement();
+            Element tag = (Element) element.getElementsByTagName("link").item(0);
+
+            strings.write("ALANNE");
+            strings.close();
+
+
+            //replaceLinha(new File("index.html"), "<link>", "<link/>");
+                /*Element element = (Element) index.getDocumentElement();
+                Element link = (Element) element.getElementsByTagName("<link></link>");
+
+                Node node = link.appendChild(link);
+                System.out.println(node);*/
+                /*Element form = (Element) index.getElementById("form");
+                Element a = (Element) form.getElementsByTagName("a").item(1);
+                Element href = (Element) a.getElementsByTagName("href").item(1);
+                System.out.println(href.getAttribute("href"));*/
         }
     }
 }
+
+//MÉTODO QUE ALTERA LINHA
+
+    /*public static void replaceLinha(File f, String linhaAlterar, String linhaAlterada) {
+        File nf = new File("index.html");
+        FileWriter fw = null;
+        Scanner s = null;
+        try {
+            fw = new FileWriter(nf);
+            s = new Scanner(f);
+
+            while (s.hasNextLine()) {
+                String linha = s.nextLine();
+
+                linha = linha.replace(linhaAlterar, linhaAlterada);
+
+                try {
+                    fw.write(linha + System.getProperty("line.separator"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fw.close();
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        f.delete();
+        nf.renameTo(f);
+    }
+}*/
